41b8219241666abe2b2fa3a434271761eee0f97f
Valentin Goxhaj <valentinogoxhaj@gmail.com>
2025-02-25 18:43:18 +0100

    fixed bug in login page

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { fadeSlide } from '../../../animations/fadeslide';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-footer',
     imports: [CommonModule],
     templateUrl: './footer.component.html',
     styles: ``,
-    animations: [fadeSlide]
+    animations: [fadeSlide],
 })
 export class footerComponent {
+    constructor(private router: Router) {}
 
+    login() {
+        this.router.navigate(['/login'])
+    }
 }
